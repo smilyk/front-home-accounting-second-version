604323a3c5232de182808828627fc8499dd41426
smilyk <smilyk1982@gmail.com>
2021-03-09 17:38:11 +0200

    change main page

--- src/app/shared/main/main.component.ts
+++ src/app/shared/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Redirect} from '../../model/Redirect';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  login() {
+    this.router.navigate([Redirect.LOGIN]);
+  }
+
+  register() {
+    this.router.navigate([Redirect.REGISTER]);
+  }
 }
